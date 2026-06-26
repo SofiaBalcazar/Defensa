4e846cac8b8346af994223b818b5052d4c666c24
Cesariño <cesargabrielolopez77@gmail.com>
2026-06-26 09:19:28 -0300

    configiguracion inicial views y en urls

--- Project/Api/urls.py
+++ Project/Api/urls.py
@@ -2,5 +2,7 @@ from django.contrib import path
 from .views import *
 
 urlpatterns = [
-    path('',home, name='Inicio'),
+    path('',Home, name='Inicio'), 
+    path('registro/',Registro,name="Registro"),
+    path('integrantes/',Integrantes,name="integrantes"),
 ]
--- Project/Api/views.py
+++ Project/Api/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from .forms import *
 
-# Create your views here.
-def home(request):
-    return render(request, 'Base.html')
+# def ---> Funcion
+# ---> Request : Retorna la Pagina Web
+def Home(request):
+    # -->Cada vez que llamemos a "Home"
+    # --> estamos hablando de la pagina Base.html
+    return render(request,'Base.html')
 
+def Registro(request):
+    data={
+        'Formulario':FormularioRegistro()
+    }
+    # --->Si el metodo de HTML es post
+    if request.method=="POST":
+        # --> Guarda lo que registro a la Base de DATOS
+        query=FormularioRegistro(data=request.POST,files=request.FILES)
+        if query.is_valid():
+            query.save()
+            data["Mensaje"]="Datos Registrados"
+        else:
+            data['Mensaje']="No se pudo Registrar"
+    return render(request,'Pages/Registro.html',data)
+
+def Integrantes(request):
+    return render(request,'Pages/Integrantes.html')
